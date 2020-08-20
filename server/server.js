ab25f0d616688ab0a2f2842d23a5a5d3a54eae61
Karsten Rabe <ravenseyesolutions@gmail.com>
2020-08-19 23:30:18 -0400

    feat(initial): add header and footer and deps, scripts, etc

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = process.env.PORT || 9001;
+const path = require('path');
+
+// Middleware:
+process.env.NODE_ENV === 'production'
+  ? app.use(morgan('common'))
+  : app.use(morgan('dev'));
+
+// Tell node where to serve static files from
+app.use(express.static(path.join(__dirname, '../client')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.use(function (req, res) {
+  res.status(418).send("I'm a teapot.");
+});
+
+app.listen(PORT).on('listening', () => {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
