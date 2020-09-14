ed08f4312fa1e8490045fd4f101978d396206e5c
Alexandra Ferdinand <alexxkf@gmail.com>
2020-09-14 15:53:32 -0400

    add path to alex profile page

--- server/server.js
+++ server/server.js
@@ -1,67 +1,71 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 9001;
-const path = require('path');
-const api = require('./api');
+const path = require("path");
+const api = require("./api");
 
 // Middleware:
-process.env.NODE_ENV === 'production'
-  ? app.use(morgan('common'))
-  : app.use(morgan('dev'));
+process.env.NODE_ENV === "production"
+  ? app.use(morgan("common"))
+  : app.use(morgan("dev"));
 
 // Tell node where to serve static files from
-app.use(express.static(path.join(__dirname, '../client/assets')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+app.use(express.static(path.join(__dirname, "../client/assets")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
 // API routes
-app.use('/api', api);
+app.use("/api", api);
 
-app.get('/profiles/karsten', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/karsten.html'));
+app.get("/profiles/karsten", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/karsten.html"));
 });
 
-app.get('/profiles/tom', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/tom.html'));
+app.get("/profiles/tom", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/tom.html"));
 });
 
-app.get('/profiles/jon', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/jon.html'));
+app.get("/profiles/jon", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/jon.html"));
 });
 
-app.get('/profiles/james', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/james.html'));
+app.get("/profiles/james", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/james.html"));
 });
 
-app.get('/profiles/sammantha', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/sammantha.html'));
+app.get("/profiles/sammantha", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/sammantha.html"));
 });
-app.get('/profiles/dave', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dave.html'));
-});
-
-app.get('/profiles/madison', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/madison.html'));
+app.get("/profiles/dave", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dave.html"));
 });
 
-app.get('/profiles/dodge', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dodge.html'));
+app.get("/profiles/madison", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/madison.html"));
 });
 
-app.get('/profiles/dan', (req, res) => {
+app.get("/profiles/dodge", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dodge.html"));
+});
+
+app.get("/profiles/dan", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dan.html"));
 });
 
-app.get('/profiles/maria', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/maria.html'));
+app.get("/profiles/maria", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/maria.html"));
 });
 
-app.use(function (req, res) {
+app.get("/profiles/alex", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/alex.html"));
+});
+
+app.use(function(req, res) {
   res.status(418).send("I'm a teapot.");
 });
 
-app.listen(PORT).on('listening', () => {
+app.listen(PORT).on("listening", () => {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
